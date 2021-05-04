9da9d72d41a247d8119855676b1f2add7971090e
PaulineTeoulle <pa.teoulle@gmail.com>
2021-05-04 15:02:33 +0200

    Modification d'un critère

--- API/models/Critere.php
+++ API/models/Critere.php
@@ -54,5 +54,38 @@ class Critere
         return false;
     }
 
+    public function modifierAvecInformations()
+    {
+        $sql = "UPDATE a_critere SET Libelle =:libelle, Informations =:informations WHERE ID_Critere = :idCritere";
+        $query = $this->connexion->prepare($sql);
+
+        $this->libelle = htmlspecialchars(strip_tags($this->libelle));
+        $this->informations = htmlspecialchars(strip_tags($this->informations));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $query->bindParam(":libelle", $this->libelle);
+        $query->bindParam(":informations", $this->informations);
+        $query->bindParam(":idCritere", $this->id);
+        if ($query->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function modifierSansInformations()
+    {
+        $sql = "UPDATE a_critere SET Libelle =:libelle WHERE ID_Critere = :idCritere";
+        $query = $this->connexion->prepare($sql);
+
+        $this->libelle = htmlspecialchars(strip_tags($this->libelle));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $query->bindParam(":libelle", $this->libelle);
+        $query->bindParam(":idCritere", $this->id);
+        if ($query->execute()) {
+            return true;
+        }
+        return false;
+    }
 
 }
