3dbf04af7994c529036624d81eaa20d0b8d7d8dd
PaulineTeoulle <pa.teoulle@gmail.com>
2021-05-05 10:51:12 +0200

    Ajout de méthode

---
+++ API/Controllers/methode/creerMethode.php
@@ -0,0 +1,47 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    include_once '../../config/Database.php';
+    include_once '../../models/Methode.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+    $methode = new Methode($db);
+
+    $donnees = json_decode(file_get_contents("php://input"));
+    if (!empty($donnees->libelle) && !empty($donnees->description) && !empty($donnees->effectif_preconise) && !empty($donnees->donnees_produites) &&
+        !empty($donnees->type_analyse) && !empty($donnees->type_methode) && !empty($donnees->exemple) && !empty($donnees->id_decision)) {
+        $methode->libelle = $donnees->libelle;
+        $methode->description = $donnees->description;
+        $methode->effectif_preconise = $donnees->effectif_preconise;
+        $methode->donnees_produites = $donnees->donnees_produites;
+        $methode->type_analyse = $donnees->type_analyse;
+        $methode->type_methode = $donnees->type_methode;
+        $methode->exemple = $donnees->exemple;
+        $methode->id_decision = $donnees->id_decision;
+
+        if ($methode->creer()) {
+            $methodeResults = [
+                "libelle" => $methode->libelle,
+                "description" => $methode->description,
+                "effectif_preconise" => $methode->effectif_preconise,
+                "donnees_produites" => $methode->donnees_produites,
+                "type_analyse" => $methode->type_analyse,
+                "type_methode" => $methode->type_methode,
+                "exemple" => $methode->exemple,
+                "id_decision" => $methode->id_decision
+            ];
+            echo json_encode(["message" => "L'ajout a été effectué"]);
+        } else {
+            echo json_encode(["message" => "L'ajout n'a pas été effectué"]);
+        }
+
+    }
+} else {
+    echo json_encode(["message" => "La méthode n'est pas autorisée"]);
+}
--- API/models/Methode.php
+++ API/models/Methode.php
@@ -30,4 +30,36 @@ class Methode
         return $result;
     }
 
+    public function creer()
+    {
+
+        $sql = "INSERT INTO a_methode
+                SET Libelle=:libelle, Description=:description, Effectif_preconise=:effectif_preconise, Donnees_produites=:donnees_produites, Type_analyse=:type_analyse,
+                Type_methode=:type_methode,Exemple=:exemple, ID_Decision=:id_decision";
+        $query = $this->connexion->prepare($sql);
+
+        $this->libelle = htmlspecialchars(strip_tags($this->libelle));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->effectif_preconise = htmlspecialchars(strip_tags($this->effectif_preconise));
+        $this->donnees_produites = htmlspecialchars(strip_tags($this->donnees_produites));
+        $this->type_analyse = htmlspecialchars(strip_tags($this->type_analyse));
+        $this->type_methode = htmlspecialchars(strip_tags($this->type_methode));
+        $this->exemple = htmlspecialchars(strip_tags($this->exemple));
+        $this->id_decision = htmlspecialchars(strip_tags($this->id_decision));
+
+        $query->bindParam(":libelle", $this->libelle);
+        $query->bindParam(":description", $this->description);
+        $query->bindParam(":effectif_preconise", $this->effectif_preconise);
+        $query->bindParam(":donnees_produites", $this->donnees_produites);
+        $query->bindParam(":type_analyse", $this->type_analyse);
+        $query->bindParam(":type_methode", $this->type_methode);
+        $query->bindParam(":exemple", $this->exemple);
+        $query->bindParam(":id_decision", $this->id_decision);
+
+        if ($query->execute()) {
+            return true;
+        }
+        return false;
+    }
+
 }
