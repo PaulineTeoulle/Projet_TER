c2cc7d2167b7510c0ad9555f2d3ef397581cd373
PaulineTeoulle <pa.teoulle@gmail.com>
2021-05-05 11:06:57 +0200

    Suppression de ressource

--- API/models/Ressource.php
+++ API/models/Ressource.php
@@ -15,10 +15,22 @@ class Ressource
 
     public function lire()
     {
-        $sql = "SELECT * FROM " . $this->table;
+        $sql = "SELECT * FROM a_ressource";
         $query = $this->connexion->prepare($sql);
         $query->execute();// On retourne le résultat
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
         return $result;
+    }
+
+    public function supprimer()
+    {
+        $sql = " DELETE FROM a_ressource WHERE ID_Ressource=:id_ressource";
+        $query = $this->connexion->prepare($sql);
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $query->bindParam(":id_ressource", $this->id);
+        if ($query->execute()) {
+            return true;
+        }
+        return false;
     }
 }
