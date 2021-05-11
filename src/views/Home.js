b579186f162acaad205d555ff6129d31c585570d
PaulineTeoulle <pa.teoulle@gmail.com>
2021-05-11 15:21:49 +0200

    Homepage + css

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import logo from '../public/logothedre.png';
+import axios from "axios";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPen} from "@fortawesome/free-solid-svg-icons";
 
-function Home() {
-    return (
-        <div className="Home">
-            <h1>Home page</h1>
-        </div>
-    );
+export default class Home extends React.Component { // Tell webpack this JS file uses this image
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: null
+        }
+    }
+
+    componentDidMount() {
+        if (this.state.description === null) {
+            axios.get('http://localhost/Projet_TER/API/Controllers/accueil/lire.php')
+                .then(response => {
+                    this.setState({description: response.data['description']});
+                })
+                .catch(error => console.log(error))
+        }
+    };
+
+    render() {
+        if (this.state.description === null) return (<p>Loading...</p>);
+        else return (
+            <div className="Home">
+                <div className="Logo">
+                    <img src={logo} alt={'Logo Thedre'}/>
+                </div>
+                <div className="Accueil">
+                    <h3> Contents <FontAwesomeIcon icon={faPen}/></h3>
+                    <p>{this.state.description}</p>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default Home;
