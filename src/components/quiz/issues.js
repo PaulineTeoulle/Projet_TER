8edb133842ff0c7666f599b47ab3525bb4065890
PaulineTeoulle <pa.teoulle@gmail.com>
2021-05-14 15:47:50 +0200

    Commit

--- src/components/quiz/issues.js
+++ src/components/quiz/issues.js
@@ -12,32 +12,32 @@ function Issues(props) {
             allChoices.push($(this).val());
         });
 
-        if (checkedBox) {
+        if(checkedBox){
             let decision = props.decisions.find(decision => decision.ID_Decision === checkedBox)
             props.changeData(decision.ID_Critere_sortant, decision, allChoices);
         }
     }
 
-    if (props.issue && props.decisions) {
+    if(props.issue && props.decisions){
         return (
             <div className="Issues">
                 <h3>{props.issue.Libelle}</h3>
                 <p className="step">Question {props.step}</p>
                 <form action="#">
-                    {props.decisions.map((decision, i) => {
-                        // if(decision.ID_Critere_sortant == null){
-                        //     // 0 == end node
-                        //     decision.ID_Critere_sortant = 0;
-                        // }
-                        return (
-                            <div key={decision.ID_Decision}>
-                                <input type="radio" name="radio-group" value={decision.ID_Decision}/>
-                                <label>{decision.Libelle}</label>
-                            </div>
-                        )
-                    })}
-                </form>
-                <button onClick={next}>Next<FontAwesomeIcon className="icon" icon={faChevronRight}/></button>
+                {props.decisions.map((decision, i) => {     
+                    // if(decision.ID_Critere_sortant == null){
+                    //     // 0 == end node
+                    //     decision.ID_Critere_sortant = 0;
+                    // }
+                    return (
+                        <div key={decision.ID_Decision}>
+                            <input type="radio" name="radio-group" value={decision.ID_Decision}/>
+                            <label>{decision.Libelle}</label>
+                        </div>
+                    ) 
+                })}
+                </form>    
+                <button onClick={next}>Next<FontAwesomeIcon className="icon" icon={faChevronRight} /></button>      
             </div>
         );
     } else {
