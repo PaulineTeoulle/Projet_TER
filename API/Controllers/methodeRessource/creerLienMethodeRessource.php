5254f6f2e9c58bc01339c6dcd20f3c99e3e44e46
PaulineTeoulle <pa.teoulle@gmail.com>
2021-05-28 14:35:11 +0200

    Add link between Methode and Ressource into db, add delete link

---
+++ API/Controllers/methodeRessource/creerLienMethodeRessource.php
@@ -0,0 +1,37 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    include_once '../../config/Database.php';
+    include_once '../../models/MethodeRessource.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+    $lienMethodeRessource = new MethodeRessource($db);
+    $donnees = json_decode(file_get_contents("php://input"));
+
+    if (!empty($donnees->id_methode_ressource) && !empty($donnees->id_methode) && !empty($donnees->id_ressource)) {
+        $lienMethodeRessource->id = $donnees->id_methode_ressource;
+        $lienMethodeRessource->id_methode = $donnees->id_methode;
+        $lienMethodeRessource->id_ressource = $donnees->id_ressource;
+        if ($lienMethodeRessource->creer()) {
+            $lienResult = [
+                "id" => $lienMethodeRessource->id,
+                "id_methode" => $lienMethodeRessource->id_methode,
+                "id_ressource" => $lienMethodeRessource->id_ressource,
+
+            ];
+            echo json_encode(["lienResult" => $lienResult]);
+            echo json_encode(["message" => "L'ajout a été effectué"]);
+        } else {
+            echo json_encode(["message" => "L'ajout n'a pas été effectué"]);
+        }
+    }
+} else {
+    echo json_encode(["message" => "La méthode n'est pas autorisée"]);
+}
--- API/models/MethodeRessource.php
+++ API/models/MethodeRessource.php
@@ -15,7 +15,8 @@ class MethodeRessource
 
     public function lire()
     {
-        $sql = "SELECT * FROM a_methoderessource";
+        $sql = "SELECT * 
+                FROM a_methoderessource";
         $query = $this->connexion->prepare($sql);
         $query->execute();
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
@@ -24,12 +25,43 @@ class MethodeRessource
 
     public function lireRessourcesMethode()
     {
-        $sql = "SELECT r.ID_Ressource, r.Nom, r.Fichier FROM a_ressource r INNER JOIN a_methoderessource mr ON r.ID_Ressource = mr.ID_Ressource WHERE mr.ID_Methode=:id_methode";
+        $sql = "SELECT r.ID_Ressource, r.Nom, r.Fichier 
+                FROM a_ressource r INNER JOIN a_methoderessource mr ON r.ID_Ressource = mr.ID_Ressource 
+                WHERE mr.ID_Methode=:id_methode";
         $query = $this->connexion->prepare($sql);
         $this->id_methode = htmlspecialchars(strip_tags($this->id_methode));
         $query->bindParam(":id_methode", $this->id_methode);
         $query->execute();
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
         return $result;
+    }
+
+    public function creer()
+    {
+        $sql = "INSERT INTO a_methoderessource
+                SET ID_MethodeRessource=:id_methode_ressource, ID_Methode=:id_methode, ID_Ressource=:id_ressource ";
+        $query = $this->connexion->prepare($sql);
+
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->id_methode = htmlspecialchars(strip_tags($this->id_methode));
+        $this->id_ressource = htmlspecialchars(strip_tags($this->id_ressource));
+        $query->bindParam(":id_methode_ressource", $this->id);
+        $query->bindParam(":id_methode", $this->id_methode);
+        $query->bindParam(":id_ressource", $this->id_ressource);
+        if ($query->execute()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function supprimer()
+    {
+        $sql = "DELETE FROM a_methoderessource ";
+        $query = $this->connexion->prepare($sql);
+
+        if ($query->execute()) {
+            return true;
+        }
+        return false;
     }
 }
