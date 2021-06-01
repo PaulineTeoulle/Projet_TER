f38f2cc29afda5779130055863aaa2eb7a0198a4
PaulineTeoulle <pa.teoulle@gmail.com>
2021-06-01 10:56:37 +0200

    Add comments of Database

--- API/config/Database.php
+++ API/config/Database.php
@@ -1,13 +1,40 @@
 <?php
 
+/**
+ * Class Database
+ * @Goal : Connect to database
+ * @UsedByModule : Controllers (all)
+ * @ModuleUsed : None
+ * @VisibleVariables : $connexion
+ * @VisibleProcedures : getConnection()
+ */
 class Database
 {
+    /**
+     * @var null connexion of database
+     */
     public $connexion;
+    /**
+     * @var string host of database
+     */
     private $host = "localhost";
+    /**
+     * @var string database name
+     */
     private $db_name = "api_matui";
+    /**
+     * @var string database user
+     */
     private $username = "root"; //"adminMatui";
+    /**
+     * @var string database password
+     */
     private $password = ""; //"Pm2Mv1MDRT!";
 
+    /**
+     * Connect to database with PDO
+     * @return PDO|null
+     */
     public function getConnection()
     {
         $this->connexion = null;
