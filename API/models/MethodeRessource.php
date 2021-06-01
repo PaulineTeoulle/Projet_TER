ff6e485996704fca36d06abb16325ad908213b97
PaulineTeoulle <pa.teoulle@gmail.com>
2021-06-01 13:51:41 +0200

    Add comments of MethodeRessource

--- API/models/MethodeRessource.php
+++ API/models/MethodeRessource.php
@@ -1,18 +1,45 @@
 <?php
 
+/**
+ * Class MethodeRessource
+ * @Goal : Read, Create, Delete link between methode and ressource
+ * @UsedByModule : Controllers/methode, creerArbre, lireArbre
+ * @ModuleUsed : None
+ * @VisibleVariables : $id,$libelle,$description,$effectif_preconise,$donnees_produites,$type_analyse,$type_methode,$exemple,$id_decision,$x,$y
+ * @VisibleProcedures : lire(), lireRessourcesMethode(), creer(), supprimer()
+ */
 class MethodeRessource
 {
+    /**
+     * @var int id of a link between a methode and a ressource
+     */
     public $id;
+    /**
+     * @var int id of a methode
+     */
     public $id_methode;
+    /**
+     * @var int id of a ressource
+     */
     public $id_ressource;
+    /**
+     * @var PDO|null connexion to database
+     */
     private $connexion;
-    private $table = "a_methoderessource";
 
+    /**
+     * MethodeRessource constructor.
+     * @param $db
+     */
     public function __construct($db)
     {
         $this->connexion = $db;
     }
 
+    /**
+     * Read all links between methode and ressource
+     * @return mixed
+     */
     public function lire()
     {
         $sql = "SELECT * 
@@ -24,6 +51,10 @@ class MethodeRessource
         return $result;
     }
 
+    /**
+     * Read all ressources linked to a methode
+     * @return mixed
+     */
     public function lireRessourcesMethode()
     {
         $sql = "SELECT r.ID_Ressource, r.Nom, r.Fichier 
@@ -40,6 +71,10 @@ class MethodeRessource
         return $result;
     }
 
+    /**
+     * Create a link between methode and ressource
+     * @return bool
+     */
     public function creer()
     {
         $sql = "INSERT INTO a_methoderessource
@@ -59,6 +94,10 @@ class MethodeRessource
         return false;
     }
 
+    /**
+     * Delete all links between methode and ressource
+     * @return bool
+     */
     public function supprimer()
     {
         $sql = "DELETE FROM a_methoderessource ";
