79d08005e0cea4d7cae6ed03c5d5ac4f2d7948c2
PaulineTeoulle <pa.teoulle@gmail.com>
2021-06-07 14:06:35 +0200

    Add read ressource

---
+++ API/Controllers/ressource/lire.php
@@ -0,0 +1,20 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    include_once '../../config/Database.php';
+    include_once '../../models/Ressource.php';
+
+    $database = new Database();
+    $db = $database->getConnection();
+
+    $ressource = new Ressource($db);
+    $ressources['ressources'] = $ressource->lire();
+    echo json_encode(["ressources" =>  $ressources['ressources']]);
+} else {
+    echo json_encode(["Message" => "Unauthorised method"]);
+}
