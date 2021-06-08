4bc3a590b296a98b41b5a445aa49bbab53ca18c8
Corentin <corentin25200@gmail.com>
2021-06-08 11:28:01 +0200

    fix(navbar)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react';
 import {Link} from 'react-router-dom';
+import logo from "../public/logothedre.png";
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {
@@ -25,48 +26,51 @@ const Nav = () => {
     };
     return (
         <div className="Navigation">
-            <nav>
-                <ul>
-                    <Link to="/">
-                        <li><FontAwesomeIcon className="icon" icon={faHome}/></li>
-                    </Link>
-                    <Link to="/quiz">
-                        <li><FontAwesomeIcon className="icon" icon={faQuestion}/></li>
-                    </Link>
-
-                    {!isAuthenticated && (<>
-                        <Link to="/login">
-                            <li><FontAwesomeIcon className="icon" icon={faUser}/></li>
+            <div>
+                <img src={logo} alt={'Logo Thedre'}/>
+                <nav>
+                    <ul>
+                        <Link to="/">
+                            <li><FontAwesomeIcon className="icon" icon={faHome}/></li>
                         </Link>
-                    </>)}
+                        <Link to="/quiz">
+                            <li><FontAwesomeIcon className="icon" icon={faQuestion}/></li>
+                        </Link>
 
-                    {userRole === 'administrator' && (<> <Link to="/manageTree">
-                        <li><FontAwesomeIcon className="icon" icon={faProjectDiagram}/></li>
-                    </Link>
+                        {!isAuthenticated && (<>
+                            <Link to="/login">
+                                <li><FontAwesomeIcon className="icon" icon={faUser}/></li>
+                            </Link>
+                        </>)}
 
-                        <Link to="/fileUpload">
-                            <li><FontAwesomeIcon className="icon" icon={faFileUpload}/></li>
-                        </Link></>)}
-
-                    {userRole === 'super-admin' && (<>
-                        <Link to="/manageTree">
+                        {userRole === 'administrator' && (<> <Link to="/manageTree">
                             <li><FontAwesomeIcon className="icon" icon={faProjectDiagram}/></li>
                         </Link>
-                        <Link to="/manageUsers">
-                            <li><FontAwesomeIcon className="icon" icon={faUserCog}/></li>
-                        </Link>
-                        <Link to="/fileUpload">
-                            <li><FontAwesomeIcon className="icon" icon={faFileUpload}/></li>
-                        </Link>
-                    </>)}
 
-                    {isAuthenticated && (<>
-                        <Link to="/logout">
-                            <li onClick={handleLogout}><FontAwesomeIcon className="icon" icon={faSignOutAlt}/></li>
-                        </Link>
-                    </>)}
-                </ul>
-            </nav>
+                            <Link to="/fileUpload">
+                                <li><FontAwesomeIcon className="icon" icon={faFileUpload}/></li>
+                            </Link></>)}
+
+                        {userRole === 'super-admin' && (<>
+                            <Link to="/manageTree">
+                                <li><FontAwesomeIcon className="icon" icon={faProjectDiagram}/></li>
+                            </Link>
+                            <Link to="/manageUsers">
+                                <li><FontAwesomeIcon className="icon" icon={faUserCog}/></li>
+                            </Link>
+                            <Link to="/fileUpload">
+                                <li><FontAwesomeIcon className="icon" icon={faFileUpload}/></li>
+                            </Link>
+                        </>)}
+
+                        {isAuthenticated && (<>
+                            <Link to="/logout">
+                                <li onClick={handleLogout}><FontAwesomeIcon className="icon" icon={faSignOutAlt}/></li>
+                            </Link>
+                        </>)}
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 }
